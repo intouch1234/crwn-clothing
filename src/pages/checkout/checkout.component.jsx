748e2a3ab8d9ee32ca09cccdb7cb85f3da3c921a
unknown <intouchbroesevangroenou@gmail.com>
2020-05-24 23:33:42 +0700

    move shop data to reducer and create selector

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,35 +5,39 @@ import {connect} from 'react-redux';
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors'; 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
-const CheckoutPage = ({ cartItems, total }) => (
-    <div className='checkout-page'>
-      <div className='checkout-header'>
-        <div className='header-block'>
-          <span>Product</span>
+const CheckoutPage = ({cartItems,total})=>(
+    <div className="checkout-page">
+        <div className="checkout-header">
+            <div className="header-block">
+                <span>Product</span>
+            </div>
+            <div className="header-block">
+                <span>Discription</span>
+            </div>
+            <div className="header-block">
+                <span>Quantity</span>
+            </div>
+            <div className="header-block">
+                <span>Price</span>
+            </div>
+            <div className="header-block">
+                <span>Remove</span>
+            </div>
         </div>
-        <div className='header-block'>
-          <span>Description</span>
+        {
+            cartItems.map(cartItem=>(
+                <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+            ))
+        }
+        <div className="total">
+            <span>TOTAL: ${total}</span>
         </div>
-        <div className='header-block'>
-          <span>Quantity</span>
-        </div>
-        <div className='header-block'>
-          <span>Price</span>
-        </div>
-        <div className='header-block'>
-          <span>Remove</span>
-        </div>
-      </div>
-      {cartItems.map(cartItem => (
-        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-      ))}
-      <div className='total'>TOTAL: ${total}</div>
     </div>
-  );
-  
-  const mapStateToProps = createStructuredSelector({
+);
+
+const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems,
     total: selectCartTotal
-  });
-  
-  export default connect(mapStateToProps)(CheckoutPage);
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import Shop_Data from './shop.data';
-import CollectionPreview from '../../components/preview-collection/collection-preview.component';
+import CollectionsOverview from '../../components/collection-overview/collection-overview.component';
+import {Route} from 'react-router-dom';
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-    constructor (props) {
-        super();
-        this.state ={
-            collections: Shop_Data
-        };
-    }
-    render() {
-        const {collections} = this.state;
-        return (<div className='shop-page'>
-            {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-            ))}
-        </div>);
-    }
-}
+const ShopPage = ({match}) => (
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`}component = {CollectionsOverview}/>
+            <Route path={`${match.path}/:collectionId`}component={CollectionPage}/>
+        </div>
+        );
 
 export default ShopPage;
